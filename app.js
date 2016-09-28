caa01cded54186504e6f138abc8161c2467944fc
Avinash Varma Penmetsa <avinashvarmap@gmail.com>
2016-09-28 21:29:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express'),
     http = require('http').Server(app),
     io = require('socket.io')(http);
 
+var server_port = 8080;
+var server_ip_address = '127.0.0.1';
+
 var GameController = require('./scripts/gameController.js')
 
 app.use(express.static(__dirname + '/public'));
@@ -11,7 +14,9 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-http.listen(9001);
+http.listen(server_port, server_ip_address, function() {
+  console.log("Listening on " + server_ip_address + ", port " + server_port);
+});
 
 var gameController = new GameController(io);
 gameController.init();
